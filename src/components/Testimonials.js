a0e60b0bc26d9a5afa5bcc6f93cdc14cf99c2ba0
mitch-json-2 <mitchmacharia@students.uonbi.ac.ke>
2023-09-11 21:29:18 +0300

    changing testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -10,19 +10,19 @@ function Testimonials() {
             
             <Carousel autoPlay={true} swipeable={false} infiniteLoop={true} interval={3000} showStatus={false} showIndicators={false} transitionTime={500}>
                 <div className="carousel-slide-caption">
-                    <h3>Looking back at the 90-year old woman in the mirror, we won’t be wishing we’d been busier… we’ll be spending time in the peak memories of our lives.</h3>
+                    <h3>The Kilimanjaro experience was nothing short of magical. The camaraderie among fellow hikers, the knowledgeable guides, and the stunning vistas made this journey unforgettable. Truly a bucket-list item for any nature lover!</h3>
                     <div className="testimonial-source">
-                        <h3><strong>Kari</strong></h3>
+                        <h3><strong>Mark</strong></h3>
                     </div>
                 </div>
                 <div className="carousel-slide-caption">
-                    <h3>Looking back at the 90-year old woman in the mirror, we won’t be wishing we’d been busier… we’ll be spending time in the peak memories of our lives.</h3>
+                    <h3>Hiking Kilimanjaro was a soul-stirring experience. The sheer determination and encouragement from our guides and fellow hikers pushed us to new heights, both physically and mentally. The sunrise from the summit was an indescribable reward.</h3>
                     <div className="testimonial-source">
                         <h3><strong>Arnold</strong></h3>
                     </div>
                 </div>
                 <div className="carousel-slide-caption">
-                    <h3>Looking back at the 90-year old woman in the mirror, we won’t be wishing we’d been busier… we’ll be spending time in the peak memories of our lives.</h3>
+                    <h3>Kilimanjaro was a true test of perseverance and a triumph of the human spirit. The sense of accomplishment upon reaching the Uhuru Peak was unlike anything else. The memories and friendships forged during this journey will last a lifetime.</h3>
                     <div className="testimonial-source">
                         <h3><strong>Henry</strong></h3>
                     </div>
